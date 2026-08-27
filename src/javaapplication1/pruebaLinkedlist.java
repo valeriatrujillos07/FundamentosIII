2ae4bb20875550faab81c72b77a18fe444bd851f
valeriatrujillos07 <valeriatrujillos07@gmail.com>
2026-08-27 12:53:16 -0700

    Actualizar

--- src/javaapplication1/pruebaLinkedlist.java
+++ src/javaapplication1/pruebaLinkedlist.java
@@ -26,6 +26,7 @@ class Student{
 
    class LnkdLst{
         Student head;
+        int index = 0;
         
         public LnkdLst(){
         this.head = null;
@@ -48,7 +49,7 @@ class Student{
         
         public void print(){
         Student current = this.head;
-        while(current.next != null){ 
+        while(current != null){ 
         System.out.println(current.name + "-" + current.score);
         current = current.next;
         }
@@ -66,23 +67,57 @@ class Student{
 
         }
         
+        public void remove(int n){  
+            Student current = this.head;
+        if(current == null || n < 0  ){
+            
+             return;   
+        }  
+            int index = 0;
+            
+            while(current !=null  && index < (n-1) ){  
+            current = current.next;
+            index++;
+            }
+            if(current == null || current.next == null  ){ 
+                return;
+        }
+            if (n >= this.index){ 
+                return;
+        }
+            current.next = current.next.next;
+            this.index--;
+        }
+        
         
         
 }
+
+
+  
 
 public class pruebaLinkedList {
   public static void main(String[] args) {
   LnkdLst lista = new LnkdLst();
   lista.add("luis", 65);
-   lista.add("Ada", 65);
-    lista.add("Paul", 65);
-    System.out.println();
-    lista.print();
-    Student found = lista.retrieve("Ada");
+  lista.add("Ada", 65);
+  lista.add("Valeria", 99);
+  System.out.println("Agregados");
+  lista.print();
+  Student found = lista.retrieve("Ada");
     System.out.println("Found " + found.name + "Score " + found.score);
     found = lista.retrieve("Guido");
+    
+    
     if(found != null){
     System.out.println("Found " + found.name + "Score" + found.score);
+   
+
     }
+     
+    lista.remove(2);
+    lista.print();
+    
     }
+  
       }
